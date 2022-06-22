571ffe23548a041dedb09898b47aec05346ac435
Darleanow <99553875+Darleanow@users.noreply.github.com>
2022-06-22 02:47:36 +0200

    Big Changes

---
+++ js/anotherindex.js
@@ -0,0 +1,26 @@
+
+function openNavFirstChild() {
+  document.getElementById("1").style.width = "100%";
+}
+function openNavSecondChild() {
+	document.getElementById("2").style.width = "100%";
+}
+function openNavThirdChild() {
+	document.getElementById("3").style.width = "100%";
+}
+
+
+function closeNav1() {
+  document.getElementById("1").style.width = "0%";
+}
+function closeNav2() {
+  document.getElementById("2").style.width = "0%";
+}
+function closeNav3() {
+  document.getElementById("3").style.width = "0%";
+}
+
+function Upsize(){
+
+}
+
